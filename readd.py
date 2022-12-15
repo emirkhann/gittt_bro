509976a241679a2338de459390fb6ebe365372f5
эмирханн <modul2004@gmail.com>
2022-12-15 16:18:39 +0600

    brooo

---
+++ readd.py
@@ -0,0 +1,15 @@
+git init - инициализирует/превращает текущую директорию в git-репозиторий
+git добавить название_файла / . - отслеживание зарегистрированных/всех файлов
+git commit -m "сообщение для фиксации" - создание версии кода с сообщением
+git remote add <name_of_remote> <ssh_key> — установка контекстного репозитория с удаленным
+git push <name_of_remote> <branch_name> - отправка версии на гитхаб
+
+git init - .git
+git remote add name url
+git pull origin master
+git status
+git add
+git add .
+git commit -m "comment
+git branch
+git push origin master
